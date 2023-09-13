8f54da88ea780e099308f092e4a43374e68f9389
nwf97 <j.lespoir971@gmail.com>
2023-09-13 13:23:03 +0200

    2nd commit

--- Terre/terre06.js
+++ Terre/terre06.js
@@ -1,26 +1,10 @@
 const { argv } = require ("process");
 
-//let string = argv[2];
+let string = argv[2];
+let newString = "";
 
-//console.log(typeof(string));
-//console.log(string);
-
-/*for (let i of string) {
-	console.log(i);
-}*/
-
-/*
-for (let i = 0; i < string.length; i++) {
-	process.stdout.write(string[i]);
+for (let i = string.length -1 ; i >= 0; i--) {
+	newString = newString + string[i];
 }
-process.stdout.write('\n');
+console.log(newString);
 
-console.log("ma variable est de type: " + typeof(string));
-*/
-
-for (let i = 0; i < argv.length; i++) {
-	if ( i > 1) {
-	//console.log(argv[i].join(' '));
-	console.log(argv[i]);
-	}
-}
